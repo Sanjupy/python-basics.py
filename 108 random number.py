c56f45931a177c64e108135d052f32af87a1748e
Yegrii <viryutinyegor@gmail.com>
2022-11-11 17:53:28 +0000

    git_train

--- 108 random number.py
+++ 108 random number.py
@@ -2,3 +2,11 @@ import random
 n=int(random.random())
 print(n)
 
+from random import randint
+
+random_number = random.randint(1, 666)
+
+for i in range(1, random_number):
+    print(i)
+
+#Changed through vim terminal
